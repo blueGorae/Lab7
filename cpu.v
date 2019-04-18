c7e44ad762b4c0b2be4e70c4e53bd3997ae7698b
seongtae0205 <seongtae0205@postech.ac.kr>
2019-04-19 01:26:23 +0900

    first commit

---
+++ cpu.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ns
+`define WORD_SIZE 16    // data and address word size
+
+module cpu(Clk, Reset_N, readM1, address1, data1, readM2, writeM2, address2, data2, num_inst, output_port, is_halted);
+	input Clk;
+	wire Clk;
+	input Reset_N;
+	wire Reset_N;
+
+	output readM1;
+	wire readM1;
+	output [`WORD_SIZE-1:0] address1;
+	wire [`WORD_SIZE-1:0] address1;
+	output readM2;
+	wire readM2;
+	output writeM2;
+	wire writeM2;
+	output [`WORD_SIZE-1:0] address2;
+	wire [`WORD_SIZE-1:0] address2;
+
+	input [`WORD_SIZE-1:0] data1;
+	wire [`WORD_SIZE-1:0] data1;
+	inout [`WORD_SIZE-1:0] data2;
+	wire [`WORD_SIZE-1:0] data2;
+
+	output [`WORD_SIZE-1:0] num_inst;
+	wire [`WORD_SIZE-1:0] num_inst;
+	output [`WORD_SIZE-1:0] output_port;
+	wire [`WORD_SIZE-1:0] output_port;
+	output is_halted;
+	wire is_halted;
+	// TODO : Implement your pipelined CPU!
+
+endmodule
