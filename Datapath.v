243e9e0c1a435bb9da67ba9ba300036b3045ef27
seongtae0205 <seongtae0205@postech.ac.kr>
2019-04-21 03:43:20 +0900

    add r_data1 bit at datapath

--- Datapath.v
+++ Datapath.v
@@ -223,6 +223,7 @@ module	Datapath(clk, reset_n, readM1, address1, data1, readM2, writeM2, address2
     assign MemData_MEM_WB_in = data2;
 
     assign ALU_Result_MEM_WB_in = ALU_Result_EX_MEM_out;
+    assign r_data1_MEM_WB_in = r_data1_EX_MEM_out;
     assign rd_MEM_WB_in = rd_EX_MEM_out;
     assign MemtoReg_MEM_WB_in = MemtoReg_EX_MEM_out;
     assign RegWrite_MEM_WB_in = RegWrite_EX_MEM_out;
@@ -231,7 +232,7 @@ module	Datapath(clk, reset_n, readM1, address1, data1, readM2, writeM2, address2
 
     FlushUnit flushUnit(clk, reset_n, PCSrc_EX_MEM_out, B_OP_EX_MEM_out, B_cond_EX_MEM_out, flush_signal);
 
-    MEM_WB mem_wb(clk, reset_n, MemData_MEM_WB_in, ALU_Result_MEM_WB_in, rd_MEM_WB_in, MemtoReg_MEM_WB_in, RegWrite_MEM_WB_in, is_wwd_MEM_WB_in, is_done_MEM_WB_in, MemData_MEM_WB_out, ALU_Result_MEM_WB_out, rd_MEM_WB_out, MemtoReg_MEM_WB_out, RegWrite_MEM_WB_out, is_wwd_MEM_WB_out, is_done_MEM_WB_out );
+    MEM_WB mem_wb(clk, reset_n, MemData_MEM_WB_in, ALU_Result_MEM_WB_in, rd_MEM_WB_in, MemtoReg_MEM_WB_in, RegWrite_MEM_WB_in, is_wwd_MEM_WB_in, is_done_MEM_WB_in, r_data1_MEM_WB_in, MemData_MEM_WB_out, ALU_Result_MEM_WB_out, rd_MEM_WB_out, MemtoReg_MEM_WB_out, RegWrite_MEM_WB_out, is_wwd_MEM_WB_out, is_done_MEM_WB_out, r_data1_MEM_WB_out );
 
     assign w_data =  MemtoReg_MEM_WB_out ? MemData_MEM_WB_out : ALU_Result_MEM_WB_out;
     assign output_port = r_data1_MEM_WB_out;
