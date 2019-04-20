27765b48c5e248b148dfba40332c7fb54fc741b6
seongtae0205 <seongtae0205@postech.ac.kr>
2019-04-21 00:56:27 +0900

    pipeline done

--- PC.v
+++ PC.v
@@ -8,11 +8,11 @@ module PC(clk, reset_n, PC_in, PC_out);
     reg [`WORD_SIZE-1 : 0] PC_out;
 
     initial begin
-        PC_out = `WORD_SIZE'bz;
+        PC_out = `WORD_SIZE'b0;
     end
 
     always @(negedge reset_n) begin
-        PC_out = `WORD_SIZE'bz;
+        PC_out = `WORD_SIZE'b0;
     end
 
     always @(posedge clk) begin
